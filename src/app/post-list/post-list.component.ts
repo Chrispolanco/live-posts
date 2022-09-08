fb2cb6cf51c689cec97f74c6083a02dbf69984e3
Chrispolanco <chrispolanco@sbcglobal.net>
2022-09-07 23:37:02 -0700

    created post service moved logic to service

--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -7,29 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-list.component.css'],
 })
 export class PostListComponent implements OnInit {
-  listOfPosts: Post[] = [
-    new Post(
-      'Catherdal',
-      'To be awestruck is a common occurrance',
-      '../../assets/midJourney/38.png',
-      'test@test.com',
-      new Date()
-    ),
-    new Post(
-      'Festival',
-      'The village gathers but once every 5 years for the festival in where worries are left for the following day',
-      '../../assets/midJourney/37.png',
-      'test@test.com',
-      new Date()
-    ),
-    new Post(
-      'Zelda',
-      'The return home after a long journey',
-      '../../assets/midJourney/36.png',
-      'test@test.com',
-      new Date()
-    ),
-  ];
+  listOfPosts: Post[] = [];
 
   constructor() {}
 
