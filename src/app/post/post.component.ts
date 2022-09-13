73686bee78f898011fae9253550441143889af92
Chrispolanco <chrispolanco@sbcglobal.net>
2022-09-12 23:02:45 -0700

    added params for route in post edit

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { PostService } from './../post.service';
 import { Component, Input, OnInit } from '@angular/core';
 import { Post } from '../post.model';
@@ -10,7 +11,7 @@ import { Post } from '../post.model';
 export class PostComponent implements OnInit {
   @Input() post?: Post; 
   @Input() index: number = 0; 
-  constructor(private postService: PostService) { }
+  constructor(private postService: PostService, private router: Router) { }
 
   ngOnInit(): void {
     console.log(this.post); 
@@ -21,4 +22,7 @@ export class PostComponent implements OnInit {
     this.postService.deletePost(this.index); 
   }
 
+  onEdit(){
+    this.router.navigate(['/post-edit', this.index]); 
+  }
 }
